a27cf1c704ba11b5043d31f8619d9651a6e9f4c4
pvskp <pvincius14@gmail.com>
2022-09-07 22:07:36 -0300

    Removes code from main.go

--- main.go
+++ main.go
@@ -5,17 +5,4 @@ import (
 )
 
 func main() {
-	// app := tview.NewApplication()
-	// flex := tview.NewFlex()
-    // flexLeft := flex.AddItem(tview.NewBox().SetBorder(true).SetTitle("Left (1/2 x width of Top)"), 0, 1, false)
-    // flexRight := flex.AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (1/2 x width of Top)"), 0, 1, false)
-
-    // flexLeft.AddItem()
-
-    // flexRight.AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (1/2 x width of Top)"), 0, 1, false)
-    // flexLeft.AddItem(tview.NewBox().SetBorder(true).SetTitle("Right (1/2 x width of Top)"), 0, 1, false)
-
-	// if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
-		// panic(err)
-	// }
 }
